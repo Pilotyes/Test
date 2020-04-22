4a8c5feffe591914f2553aa35ac6cf5016d60ca4
alena <al.proud-fox@yandex.ru>
2020-04-22 22:59:24 +0300

    Третья версия программы

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
-/*Version 2:
-Расширить функционал программы так, чтобы координаты точки и радиус круга вводились пользователем с клавиатуры. Предусмотреть возможность ввода некорректных данных. Диалог с пользователем:
-"Введите координату X: "
-"Введите координату Y: "
-"Введите радиус R: "
-Закоммитить вторую версию кода
+/*Version 3:
+Вынести вычисление принадлежности точки с координатами в отдельную функцию с семантикой:
+Входящие аргументы:
+	x, y, r: float
+Возвращяемые значения:
+	ret: bool
+Закоммитить третью версию кода
 */
 
 package main
 
 import "fmt"
 
+func beBelong(x, y, r float64) bool {
+	return (x*x + y*y) <= r*r
+}
+
 func main() {
 	var x, y, r float64
-	fmt.Println("Введите координату X:")
-	fmt.Scanln(&x)
+	for {
+		fmt.Println("Введите координату X:")
+		_, err := fmt.Scanf("%f", &x)
+		if err == nil {
+			break
+		} else {
+			fmt.Println("НЕкорректное значение")
+		}
+	}
 	fmt.Println("Введите координату Y:")
 	fmt.Scanln(&y)
 	fmt.Println("Введите радиус R:")
 	fmt.Scanln(&r)
-	if (x*x + y*y) > r*r {
-		fmt.Printf("Tochka s coordinatami x = %f, y = %f - ne popadaet v krug s radiusom %f", x, y, r)
+	if beBelong(x, y, r) == false {
+		fmt.Printf("Tochka s coordinatami x = %f, y = %f - ne popadaet v krug s radiusom %f\n", x, y, r)
 	} else {
-		fmt.Printf("Tochka s coordinatami x = %f, y = %f - popadaet v krug s radiusom %f", x, y, r)
+		fmt.Printf("Tochka s coordinatami x = %f, y = %f - popadaet v krug s radiusom %f\n", x, y, r)
 	}
 }
