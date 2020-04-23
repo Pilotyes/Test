fe4e31d239211def265b12efa71c6130f615be15
alena <al.proud-fox@yandex.ru>
2020-04-23 20:19:09 +0300

    Vse gudno

--- main.go
+++ main.go
@@ -9,27 +9,55 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 func beBelong(x, y, r float64) bool {
-	return (x*x + y*y) <= r*r
+	return (math.Pow(x, 2.0) + math.Pow(y, float64(2))) <= math.Pow(r, 2.0)
 }
-
+func readFloat(str string) (float64, error) {
+	fmt.Println(str)
+	var tmpStr string
+	_, err := fmt.Scanln(&tmpStr)
+	if err != nil {
+		return float64(0), err
+	}
+	ret, err := strconv.ParseFloat(tmpStr, 64)
+	if err != nil {
+		return float64(0), err
+	}
+	return ret, nil
+}
 func main() {
 	var x, y, r float64
+	var err error
 	for {
-		fmt.Println("Введите координату X:")
-		_, err := fmt.Scanf("%f", &x)
+		x, err = readFloat("Введите координату X:")
 		if err == nil {
 			break
 		} else {
 			fmt.Println("НЕкорректное значение")
 		}
 	}
-	fmt.Println("Введите координату Y:")
-	fmt.Scanln(&y)
-	fmt.Println("Введите радиус R:")
-	fmt.Scanln(&r)
+	for {
+		y, err = readFloat("Введите координату Y:")
+		if err == nil {
+			break
+		} else {
+			fmt.Println("НЕкорректное значение")
+		}
+	}
+	for {
+		r, err = readFloat("Введитe radius R:")
+		if err == nil {
+			break
+		} else {
+			fmt.Println("НЕкорректное значение")
+		}
+	}
 	if beBelong(x, y, r) == false {
 		fmt.Printf("Tochka s coordinatami x = %f, y = %f - ne popadaet v krug s radiusom %f\n", x, y, r)
 	} else {
